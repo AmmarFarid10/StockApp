aab1651918ec2841cb1df478aae87651b509a38b
AmmarFarid10 <91678027+AmmarFarid10@users.noreply.github.com>
2022-05-29 00:48:37 +0500

    Update stockApp.py

--- stockApp.py
+++ stockApp.py
@@ -9,9 +9,15 @@ from sklearn.metrics import plot_confusion_matrix, plot_roc_curve, plot_precisio
 from sklearn.metrics import precision_score, recall_score
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('TkAgg')
+
 import seaborn as sns
 import pandas as pd
 import warnings
+from sklearn.svm import SVC
+import numpy as np
+from sklearn.metrics import accuracy_score
 warnings.filterwarnings("ignore", category=FutureWarning)
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
@@ -32,6 +38,50 @@ def getClassifier(classifier):
     return model
 
 
+def SVM(df):
+    df.index = pd.to_datetime(df['Date'])  
+        # drop The original date column
+    df = df.drop(['Date'], axis='columns')
+        # Create predictor variables
+    df['Open-Close'] = df.Open - df.Close
+    df['High-Low'] = df.High - df.Low
+        # Store all predictor variables in a variable X
+    X = df[['Open-Close', 'High-Low']]
+        # Target variables
+    y = np.where(df['Close'].shift(-1) > df['Close'], 1, 0)
+    split_percentage = 0.8
+    split = int(split_percentage*len(df))
+  
+        # Train data set
+    X_train = X[:split]
+    y_train = y[:split]
+  
+        # Test data set
+    X_test = X[split:]
+    y_test = y[split:]
+
+
+        # Support vector classifier
+    cls = SVC().fit(X_train, y_train)
+    df['Predicted_Signal'] = cls.predict(X)
+        # Calculate daily returns
+    df['Return'] = df.Close.pct_change()
+
+        # Calculate strategy returns
+    df['Strategy_Return'] = df.Return *df.Predicted_Signal.shift(1)
+        # Calculate Cumulutive returns
+    df['Cum_Ret'] = df['Return'].cumsum()
+
+        # Plot Strategy Cumulative returns 
+    df['Cum_Strategy'] = df['Strategy_Return'].cumsum()
+    plt.style.use('seaborn-darkgrid')
+    df=df.dropna()
+
+    fig,ax=plt.subplots()
+    ax.plot(df['Cum_Ret'],color='red')
+    ax.plot(df['Cum_Strategy'],color='blue')
+
+    st.pyplot(fig)
 
 
 # Title
@@ -44,6 +94,7 @@ sideBar = st.sidebar
 display = sideBar.checkbox('Display Dataset')
 
 uploaded_file = sideBar.file_uploader("Choose a CSV file")
+classifier = sideBar.selectbox('Which Classifier do you want to use?',('SVM' , 'KNN' , 'Random Forest'))
 
 
 if uploaded_file is not None:
@@ -52,12 +103,21 @@ if uploaded_file is not None:
         df=pd.read_csv(uploaded_file)
         if display:
             st.dataframe(df)
+        if classifier == 'SVM':
+            SVM(df)
+
+
+  
+
+
 
 
     elif uploaded_file.name.endswith('.xlsx'):
         df = pd.read_excel(uploaded_file)
         if display:
             st.dataframe(df)
+        if classifier == 'SVM':
+            SVM(df)
 
     else:
         sideBar.write("Please upload csv or excel files only")
